b27d9465d826009bf03dd33526f72dec84e15dc7
zsozs <vzsozs@gmail.com>
2025-09-06 07:58:21 +0200

    Fix: Final private key handling for Vercel

--- api/backend/index.js
+++ api/backend/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 const auth = new google.auth.GoogleAuth({
   credentials: {
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    private_key: process.env.GOOGLE_PRIVATE_KEY,
   },
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
