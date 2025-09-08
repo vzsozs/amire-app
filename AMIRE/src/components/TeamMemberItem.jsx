6c0ed48b7f36c0cb8ba32edf09ba2f07ab6c31cf
zsozs <vzsozs@gmail.com>
2025-09-08 08:22:42 +0200

    v1.61 - User color

--- AMIRE/src/components/TeamMemberItem.jsx
+++ AMIRE/src/components/TeamMemberItem.jsx
@@ -5,12 +5,15 @@ import { FaUserCircle, FaChevronRight } from 'react-icons/fa';
 import './TeamMemberItem.css'; // VÁLTOZÁS
 
 function TeamMemberItem({ person }) { // VÁLTOZÁS
-  const { id, name, role } = person;
+  const { id, name, role, color } = person;
+
+    // Ha valamiért nincs szín, használjunk egy alapértelmezett szürkét
+  const avatarColor = color || '#808080';
 
   return (
     <Link to={`/team/${id}`} className="team-member-item-link"> {/* VÁLTOZÁS */}
       <div className="team-member-item"> {/* VÁLTOZÁS */}
-        <FaUserCircle className="team-member-avatar" /> {/* VÁLTOZÁS */}
+        <FaUserCircle className="team-member-avatar" style={{ color: avatarColor }} /> {/* VÁLTOZÁS */}
         <div className="team-member-info"> {/* VÁLTOZÁS */}
           <h3 className="team-member-name">{name}</h3> {/* VÁLTOZÁS */}
           <p className="team-member-role">{role}</p> {/* VÁLTOZÁS */}
